4d3f7a5be81500c2e16e9bbcbed95ba9c3a2d96d
Justin Hartmann <hartmann.justin@gmail.com>
2021-03-23 17:57:27 -0500

    Sync repo

--- src/ProjectEuler3.java
+++ src/ProjectEuler3.java
@@ -7,8 +7,16 @@
 //See if those prime numbers are factors of 600,851,475,143
 //Select the largest prime factor
 
+
+
 public class ProjectEuler3 {
     public static void main(String[] args) {
+        int start = 1;
+        int end = 600851475;
+        long end1 = 600851475;
+        int end2 = 600851475;
+
+        
 
 
     }
